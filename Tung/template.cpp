e7e44d065af4ec0cbea4bb20465bb31bf32020f2
Vu Tung Anh <vu.tunganh96@gmail.com>
2017-05-07 18:37:34 +0200

    vvi added

--- Tung/template.cpp
+++ Tung/template.cpp
@@ -2,6 +2,7 @@
 #define int_in(n) int n; scanf( " %d", &n )
 #define str_in(s, l) char s[ l ]; scanf( " %s", s ); s[ l - 1 ] = 0
 #define vi vector< int >
+#define vvi vector< vector< int > >
 #define endl printf( "\n" );
 #define PB push_back
 
