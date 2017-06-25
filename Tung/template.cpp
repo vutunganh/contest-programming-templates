fac4c6d18b07b8acb0fd4c7c9209ae5180c23310
Vu Tung Anh <vu.tunganh96@gmail.com>
2017-06-25 21:29:58 +0200

    updated templates

--- Tung/template.cpp
+++ Tung/template.cpp
@@ -1,14 +1,22 @@
-#include <bits/stdc++.h>
+/* #include <bits/stdc++.h> */
 #define int_in(n) int n; scanf( " %d", &n )
+#define int_sin(n) int n; cin >> n;
+#define long_in(n) long long n; scanf( " %lld", &n )
+#define long_sin(n) long long n; cin >> n;
+#define DPRINT(X) cout << #X << ":" << X << endl;
 #define str_in(s, l) char s[ l ]; scanf( " %s", s ); s[ l - 1 ] = 0
+#define str_sin(s, l) string s; cin >> s
 #define vi vector< int >
-#define vvi vector< vector< int > >
-#define endl printf( "\n" );
+#define vl vector< long long >
+#define vii vector< vector< int > >
+#define vll vector< vector< long long > >
+#define FOR(i,a,b) for( int i = a; i < b; ++i )
 #define PB push_back
 
 using namespace std;
 
 int main( void )
 {
+  ios::sync_with_stdio( false );
   return 0;
 }
