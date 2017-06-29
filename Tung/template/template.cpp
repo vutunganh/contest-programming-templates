b3f3eac70e84b814e7e6275bf97a1ef466897da5
Vu Tung Anh <vu.tunganh96@gmail.com>
2017-06-29 18:17:22 +0200

    updated template

--- Tung/template/template.cpp
+++ Tung/template/template.cpp
@@ -1,8 +1,8 @@
-/* #include <bits/stdc++.h> */
-#define int_in(n) int n; scanf( " %d", &n )
-#define int_sin(n) int n; cin >> n;
-#define long_in(n) long long n; scanf( " %lld", &n )
-#define long_sin(n) long long n; cin >> n;
+#include <bits/stdc++.h>
+#define i_in(n) int n; scanf( " %d", &n )
+#define i_sin(n) int n; cin >> n
+#define l_in(n) long long n; scanf( " %lld", &n )
+#define l_sin(n) long long n; cin >> n
 #define DPRINT(X) cout << #X << ":" << X << endl;
 #define str_in(s, l) char s[ l ]; scanf( " %s", s ); s[ l - 1 ] = 0
 #define str_sin(s, l) string s; cin >> s
@@ -11,6 +11,9 @@
 #define vii vector< vector< int > >
 #define vll vector< vector< long long > >
 #define FOR(i,a,b) for( int i = a; i < b; ++i )
+#define UFOR(i,a,b) for( size_t i = a; i < b; ++i )
+#define aa first
+#define bb second
 #define PB push_back
 
 using namespace std;
