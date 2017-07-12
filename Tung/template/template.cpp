87ddaa4ad59fd971cfc3110fa7d71674add9f391
Vu Tung Anh <vu.tunganh96@gmail.com>
2017-07-12 20:45:55 +0200

    added map find to templates added tests to makefile

--- Tung/template/template.cpp
+++ Tung/template/template.cpp
@@ -1,21 +1,21 @@
 #include <bits/stdc++.h>
 #define i_in(n) int n; scanf( " %d", &n )
 #define i_sin(n) int n; cin >> n
-#define l_in(n) long long n; scanf( " %lld", &n )
-#define l_sin(n) long long n; cin >> n
+#define ll_in(n) long long n; scanf( " %lld", &n )
+#define ll_sin(n) long long n; cin >> n
 #define DPRINT(X) cout << #X << ":" << X << endl;
-#define str_in(s, l) char s[ l ]; scanf( " %s", s ); s[ l - 1 ] = 0
-#define str_sin(s, l) string s; cin >> s
 #define vi vector< int >
 #define vl vector< long long >
-#define vii vector< vector< int > >
-#define vll vector< vector< long long > >
+#define vvi vector< vector< int > >
+#define vvll vector< vector< long long > >
 #define FOR(i,a,b) for( int i = a; i < b; ++i )
 #define UFOR(i,a,b) for( size_t i = a; i < b; ++i )
 #define aa first
 #define bb second
 #define PB push_back
 #define MP make_pair
+#define MF(m,f) ( m.find( f ) != m.end( ) )
+#define MOD ( static_cast< int >( 1e9 + 7 ) )
 
 using namespace std;
 
