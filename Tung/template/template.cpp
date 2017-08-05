08b49bc8ad3cc9e180dd8cfdc522d3aef11c8013
Vu Tung Anh <vu.tunganh96@gmail.com>
2017-08-05 13:19:06 +0200

    updated templates

--- Tung/template/template.cpp
+++ Tung/template/template.cpp
@@ -1,28 +1,26 @@
 #include <bits/stdc++.h>
 using ll = long long;
 using ull = unsigned long long;
-#define i_in(n) int n; scanf( " %d", &n )
+#define i_in(n) int n; scanf(" %d", &n)
 #define i_sin(n) int n; cin >> n
 #define DPRINT(X) cout << #X << ":" << X << endl;
 #define vi vector< int >
 #define vvi vector< vector< int > >
-#define FOR(i,a,b) for( auto i = a; i < b; ++i )
-#define REP(n) F( 0, n )
-#define F(a,b) FOR( i, a, b )
-#define FF(a,b) FOR( j, a, b )
+#define FOR(i,a,b) for (auto i = a; i < b; ++i)
+#define REP(n) F(0,n)
+#define F(a,b) FOR(i, a, b)
+#define FF(a,b) FOR(j, a, b)
 #define aa first
 #define bb second
 #define PB push_back
 #define MP make_pair
-#define MF(m,f) m.find( f )
-#define MFB(m,f) ( m.find( f ) != m.end( ) )
 #define sc(t) static_cast< t >
-#define MOD ( sc( int )( 1e9 + 7 ) )
+#define MOD (sc(int)(1e9 + 7))
 
 using namespace std;
 
-int main( void )
+int main(void)
 {
-  ios::sync_with_stdio( false );
+  ios::sync_with_stdio(false);
   return 0;
 }
