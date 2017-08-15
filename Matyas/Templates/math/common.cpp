6ef493439264f066a33a6ec076ac4587d40ce3d8
kristja6 <kritja6@fit.cvut.cz>
2017-08-15 21:34:06 +0200

    odcitani v modulu

--- Matyas/Templates/math/common.cpp
+++ Matyas/Templates/math/common.cpp
@@ -325,3 +325,8 @@ vector<int> fastDivisors(int n, vector<int> sieve) {
   getDivs(result, divs, 1, 0);
   return result;
 }
+
+// (x - y) % k
+inline ll modsub(ll x, ll y, ll k) {
+  return (x%k - y%k + k) % k;
+}
