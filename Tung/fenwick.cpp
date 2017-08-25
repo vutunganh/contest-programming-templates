0209d87bda8728572b91cd2de4f91cf5ec4c1d8a
Vu Tung Anh <vu.tunganh96@gmail.com>
2017-08-25 16:49:36 +0200

    upraven fenwick pro krasu

--- Tung/fenwick.cpp
+++ Tung/fenwick.cpp
@@ -1,36 +1,14 @@
-#include <bits/stdc++.h>
-#define int_in(n) int n; scanf( " %d", &n )
-#define str_in(s, l) char s[ l ]; scanf( " %s", s ); s[ l - 1 ] = 0
-#define vi vector< int >
-#define endl printf( "\n" );
-#define PB push_back
-
-using namespace std;
-
-vi T;
+#define MAX 1000001
+int T[1000001]; // T[0] se nepouziva!!!
 
 void add( int i, int delta )
 {
-  while( i < T.size( ) )
-  {
-    T[ i ] += delta;
-    i += ( i & -i );
-  }
+  while( i < MAX ) T[ i ] += delta, i += ( i & -i );
 }
 
 int pref_sum( int i )
 {
   int res = 0;
-  while( i > 0 )
-  {
-    s += T[ i ];
-    i = i & ( i - 1 );
-  }
-
+  while( i > 0 ) s += T[ i ], i = i & ( i - 1 );
   return res;
-}
-
-int main( void )
-{
-  return 0;
 }
