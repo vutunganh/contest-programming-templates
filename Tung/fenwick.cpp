6aa9039ee6f5295229e4f871b037724dd1d32b52
Vu Tung Anh <vu.tunganh96@gmail.com>
2017-08-31 00:36:28 +0200

    vylepsen fenwick

--- Tung/fenwick.cpp
+++ Tung/fenwick.cpp
@@ -1,14 +1,10 @@
 #define MAX 1000001
-int T[1000001]; // T[0] se nepouziva!!!
+int T[MAX]; // T[0] se nepouziva!!!
 
-void add( int i, int delta )
-{
-  while( i < MAX ) T[ i ] += delta, i += ( i & -i );
-}
+void add(int i,int delta) { while (i<=MAX) T[i]+=delta,i+=(i&-i); }
 
-int pref_sum( int i )
-{
-  int res = 0;
-  while( i > 0 ) s += T[ i ], i = i & ( i - 1 );
-  return res;
+int pref_sum(int i) {
+  int s = 0;
+  while (i>0) s+=T[i],i=i&(i-1);
+  return s;
 }
