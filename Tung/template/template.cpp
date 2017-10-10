d7a2f147a7b245fe64a62af254676aa40a95f0ea
Vu Tung Anh <vu.tunganh96@gmail.com>
2017-10-10 11:27:54 +0200

    pridan template a konfiguraky bashe a vimu

--- Tung/template/template.cpp
+++ Tung/template/template.cpp
@@ -1,12 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using pii = pair<int, int>;
-using ll = long long;
-using ull = unsigned long long;
-using ld = double;
-using vi = vector<int>;
-using vvi = vector<vector<int>>;
 #define FOR(i,a,b) for (auto i=(a); i<(b); ++i)
 #define F(a,b) FOR(i, (a), (b))
 #define FF(a,b) FOR(j, (a), (b))
@@ -15,13 +9,20 @@ using vvi = vector<vector<int>>;
 #define PB push_back
 #define MOD ((int)1e9+7)
 #define INF (1<<30)
-#define LINF (1<<62LL)
+#define LINF (1LL<<62LL)
 #define EPS (1e-10)
 #define EQ(a,b) (fabs(a-b)<=fabs(a+b)*EPS)
+using ll = long long;
+using ull = unsigned long long;
+using ld = double;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using pii = pair<int, int>;
 
 int main(void){
   ios::sync_with_stdio(false);
   // freopen("input.txt","r",stdin);
   // freopen("output.txt","w",stdout);
+
   return 0;
 }
