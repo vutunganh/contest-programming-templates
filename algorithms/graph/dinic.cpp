81e8053c910d798c46fda0f82addaf36863cd906
Vu Tung Anh <vu.tunganh96@gmail.com>
2017-11-05 12:38:21 +0100

    pridan experimentalni init u Dinice

--- algorithms/graph/dinic.cpp
+++ algorithms/graph/dinic.cpp
@@ -1,71 +1,78 @@
-const int NODES = 5000, EDGES = 30000;
+#define NODES (5000)
+#define EDGES (30000)
 int N, M = NODES, source, sink;
 
 struct Edge {
-    int from, to;
-    long long residue;
+  int from, to;
+  long long residue;
 } edges[NODES+2*EDGES];
 int adj[NODES+2*EDGES], work_adj[NODES+2*EDGES], work_edges[NODES+2*EDGES];
 
 bool bfs() {
-    static int dist[NODES], q[NODES];
-    int qsz = 0;
+  static int dist[NODES], q[NODES];
+  int qsz = 0;
 
-    for (int i = 0; i < N; ++i) {
-        work_adj[i] = -1;
-        dist[i] = N+1;
-    }
-    int work_M  = NODES;
+  for (int i = 0; i < N; ++i) {
+    work_adj[i] = -1;
+    dist[i] = N+1;
+  }
+  int work_M  = NODES;
 
-    q[qsz++] = source;
-    dist[source] = 0;
-    for (int qi = 0; qi < qsz && dist[q[qi]] + 1 <= dist[sink]; ++qi)
-        for (int i = adj[q[qi]]; i >= 0; i = adj[i]) {
-            Edge & edge = edges[i];
-            if (edge.residue && dist[edge.from] + 1 <= dist[edge.to]) {
-                work_adj[work_M] = work_adj[edge.from];
-                work_edges[work_M] = i;
-                work_adj[edge.from] = work_M++;
+  q[qsz++] = source;
+  dist[source] = 0;
+  for (int qi = 0; qi < qsz && dist[q[qi]] + 1 <= dist[sink]; ++qi)
+    for (int i = adj[q[qi]]; i >= 0; i = adj[i]) {
+      Edge & edge = edges[i];
+      if (edge.residue && dist[edge.from] + 1 <= dist[edge.to]) {
+        work_adj[work_M] = work_adj[edge.from];
+        work_edges[work_M] = i;
+        work_adj[edge.from] = work_M++;
 
-                if (dist[edge.to] == N+1) {
-                    dist[edge.to] = dist[edge.from] + 1;
-                    q[qsz++] = edge.to;
-                }
-            }
+        if (dist[edge.to] == N+1) {
+          dist[edge.to] = dist[edge.from] + 1;
+          q[qsz++] = edge.to;
         }
-    return dist[sink] != N+1;
+      }
+    }
+  return dist[sink] != N+1;
 }
 
 long long dfs(int node, long long flow) {
-    if (!flow || node == sink)
-        return flow;
+  if (!flow || node == sink)
+    return flow;
 
-    for (int & i = work_adj[node]; i >= 0; i = work_adj[i]) {
-        int eindex = work_edges[i];
-        long long fl;
-        if (fl = dfs(edges[eindex].to, min(flow, edges[eindex].residue))) {
-            edges[eindex].residue -= fl;
-            edges[eindex^1].residue += fl;
-            return fl;
-        }
+  for (int & i = work_adj[node]; i >= 0; i = work_adj[i]) {
+    int eindex = work_edges[i];
+    long long fl;
+    if (fl = dfs(edges[eindex].to, min(flow, edges[eindex].residue))) {
+      edges[eindex].residue -= fl;
+      edges[eindex^1].residue += fl;
+      return fl;
     }
-    return 0;
+  }
+  return 0;
 }
 
 void add_edge(int from, int to, long long cap1, long long cap2) {
-    edges[M] = Edge{from, to, cap1};
-    adj[M] = adj[from];
-    adj[from] = M++;
+  edges[M] = Edge{from, to, cap1};
+  adj[M] = adj[from];
+  adj[from] = M++;
 
-    edges[M] = Edge{to, from, cap2};
-    adj[M] = adj[to];
-    adj[to] = M++;
+  edges[M] = Edge{to, from, cap2};
+  adj[M] = adj[to];
+  adj[to] = M++;
+}
+
+void init(int nodeCnt,int st,int en){
+  N=M=nodeCnt;
+  source=st;
+  sink=en;
 }
 
 long long maxflow() {
-    long long total_flow = 0, flow;
-    while (bfs())
-        while (flow = dfs(source, 1<<30))
-            total_flow += flow;
-    return total_flow;
+  long long total_flow = 0, flow;
+  while (bfs())
+    while (flow = dfs(source, 1<<30))
+      total_flow += flow;
+  return total_flow;
 }
