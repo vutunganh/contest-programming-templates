ea9e169e3e824ebb9e1c92a1e369c98130147e59
Vu Tung Anh <vu.tunganh96@gmail.com>
2017-11-21 21:17:51 +0100

    mini zmeny templatu

--- template/template.cpp
+++ template/template.cpp
@@ -13,19 +13,18 @@ using vvll=vector<vll>;
 #define FF(n) REP(j,n)
 #define aa first
 #define bb second
-#define pb push_back
+#define PB push_back
 #define MOD ((ll)1e9+7)
 #define INF (1<<30)
 #define LINF (1LL<<61)
 #define EPS (1e-10)
 #define EQ(a,b) (fabs(a-b)<=(fabs(a+b)*EPS))
-#define all(x) x.begin(),x.end()
 #define CL(A,I) memset(A,I,sizeof(A))
 struct d_{
   template<class T> d_ & operator,(const T& x){cerr<<' '<<x;return *this;}
   template<class T> d_ & operator,(const vector<T>& x){for(auto& x:x)cerr<<' '<<x;return *this;}
 }d_t;
-#define dbg(args ...) {d_t,"|",__LINE__,"|",":",args,"\n";}
+#define dbg(args ...) {d_t,__LINE__,"|",args,"\n";}
 
 int main(){
   ios::sync_with_stdio(false);cout.tie(0);cin.tie(0);
