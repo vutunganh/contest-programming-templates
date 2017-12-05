8777af18a7500c175311eea9e8d93cb42c06026e
Vu Tung Anh <vu.tunganh96@gmail.com>
2017-12-05 23:47:49 +0100

    dijkstra s ll

--- algorithms/graph/dijkstra.cpp
+++ algorithms/graph/dijkstra.cpp
@@ -1,56 +1,65 @@
-// tested http://www.spoj.com/problems/EZDIJKST/
-#define NODES 10000
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using ld=double;
+using pll=pair<ll,ll>;
+using vll=vector<ll>;
+using vpll=vector<pll>;
+using vvll=vector<vll>;
+#define FOR(i,a,b) for(ll i=a;i<(ll)b;++i)
+#define ROF(i,a,b) for(ll i=a;i>=(ll)b;--i)
+#define F(n) FOR(i,0,n)
+#define FF(n) FOR(j,0,n)
+#define aa first
+#define bb second
+#define PB push_back
+#define EQ(a,b) (fabs(a-b)<=(fabs(a+b)*EPS))
 
-struct Edge
-{
-  int u;
-  ull w;
-  bool operator<(const Edge& a) const { return w < a.w; }
+#define LINF 1LL<<61
+#define MX 10000
+
+struct Edge{
+  ll v,w; // destination and weight
+  bool operator<(const Edge& a)const{return w<a.w;}
 };
 
-int s,t,N,M; // start, target, node count, vertex count
-ull D[NODES]; // distances
-vector<Edge> G[NODES]; // graph
-int P[NODES]; // parents
+// node cnt,edge cnt,distances,parents
+ll N,M,D[MX],P[MX];
+vector<Edge> G[MX];
 
-void add_edge(int u,int v,ull w){G[u].PB({v,w});}
+void add_edge(ll u,ll v,ll w){G[u].PB({v,w});}
 
-void dijkstra(){
-  F(NODES)D[i]=INF,P[i]=-1;
+void dijkstra(ll s){
+  F(N)D[i]=LINF,P[i]=-1;
   D[s]=0;
   priority_queue<Edge> Q;
-  Q.push({s, D[s]});
-  while(!Q.empty()){
-    int v=Q.top().u;Q.pop();
-    for(const auto& e:G[v]){
-      if(D[e.u]>D[v]+e.w)
-        D[e.u]=D[v]+e.w,P[e.u]=v,Q.push(e);
+  Q.push({s,D[s]});
+  while(Q.size()){
+    auto u=Q.top().v;Q.pop();
+    for(auto e:G[u])
+      if(D[e.v]>D[u]+e.w)
+        D[e.v]=D[u]+e.w,P[e.v]=u,Q.push({e.v,D[e.v]});
+  }
+}
+
+int main(){
+  ios::sync_with_stdio(0);
+  int _;cin>>_;
+  while(_--){
+    cin>>N>>M;
+    F(MX)G[i].clear();
+    F(M){
+      ll u,v,w;
+      cin>>u>>v>>w;
+      add_edge(u-1,v-1,w);
     }
+    ll s,t;
+    cin>>s>>t;
+    --s,--t;
+    dijkstra(s);
+    if(D[t]==LINF)cout<<"NO"<<endl;
+    else cout<<D[t]<<endl;
   }
-}
-
-void solve(){
-  scanf(" %d %d",&N,&M);
-  F(NODES)G[i].clear();
-  F(M){
-    int u,v,w;
-    scanf(" %d %d %d",&u,&v,&w);
-    add_edge(u-1,v-1, w);
-  }
-  scanf(" %d %d",&s,&t);
-  s-=1;t-=1; //numbering was 1 based
-  dijkstra();
-  if(D[t]==INF)
-    puts("NO");
-  else
-    printf("%lld\n",D[t]);
-}
-
-int main(void)
-{
-  ios::sync_with_stdio(false);
-  int T;scanf(" %d",&T);
-  while(T--)solve();
 
   return 0;
 }
