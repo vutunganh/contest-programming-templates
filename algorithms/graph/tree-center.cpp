f2102827c874eb72804ae882e4febb155383d031
Matyas Kristan <matyas.kristan@gmail.com>
2019-10-31 22:01:17 +0100

    shorter tree centers using bfs

--- algorithms/graph/tree-center.cpp
+++ algorithms/graph/tree-center.cpp
@@ -1,37 +1,26 @@
-// pomocna funkce
-pair<bool, int> treeCenterRec(vector<Vertex> & g, int n, int depth, int diameter, int end) {
-  if (g[n].v) return {false, -1};
-  g[n].v = true;
-  g[n].depth = depth;
-
-  bool foundEnd = false;
-  int endIdx = -1;
-  if (n == end) {
-    foundEnd = true;
+// returns either one or two vertices
+vector<ll> treeCenters(const vector<vector<ll>> & g) {
+  ll n = g.size();
+  depth = vector<ll>(n, -1);
+  queue<ll> q;
+  F(n) if (g[i].size() == 1) {
+    q.push(i);
+    depth[i] = 0;
   }
-
-  for (int i = 0; i < g[n].e.size(); ++i) {
-    auto a = treeCenterRec(g, g[n].e[i], depth + 1, diameter, end);
-    foundEnd |= a.first;
-    if (a.second != -1) {
-      endIdx = a.second;
-      break;
+  
+  while (q.size()) {
+    ll cur = q.front();
+    q.pop();
+    F(g[cur].size()) {
+      if (depth[g[cur][i]] == -1) {
+        q.push(g[cur][i]);
+        depth[g[cur][i]] = depth[cur] + 1;
+      }
     }
   }
-
-  if (foundEnd && depth == diameter/2) {
-    endIdx = n;
-    return {true, endIdx};
-  }
-
-  return {foundEnd, endIdx};
-}
-
-// nalezne nejaky stredu stromu
-// O(n)
-int treeCenter(vector<Vertex> & g) {
-  int diameter = treeDiameter(g);
-  int result = treeCenterRec(g, startOfLongestPath, 0, diameter, endOfLongestPath).second;
-  for (int i = 0; i < g.size(); ++i) g[i].v = false;
-  return result;
+  ll mx = -1;
+  F(n) mx = max(depth[i], mx);
+  vector<ll> res;
+  F(n) if (depth[i] == mx) res.PB(i);
+  return res;
 }
